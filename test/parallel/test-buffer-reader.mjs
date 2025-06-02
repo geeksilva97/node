16112d23052856a3a13480b8c7211f10c4efc0c9
Edy Silva <edigleyssonsilva@gmail.com>
2025-06-02 10:00:29 -0300

    add buffer reader test

---
+++ test/parallel/test-buffer-reader.mjs
@@ -0,0 +1,64 @@
+import '../common/index.mjs';
+import { describe, it } from 'node:test';
+
+describe('Buffer.prototype.getReader', () => {
+  it('returns a BufferReader instance', (t) => {
+    const buf = Buffer.from([0xca, 0xfe, 0xba, 0xbe]);
+    const reader = buf.getReader();
+
+    t.equal(typeof reader.read, 'function');
+    t.equal(typeof reader.seek, 'function');
+  });
+
+  it('BufferReader.prototype.read gets n bytes from actual buffer', (t) => {
+    const buf = Buffer.from([0xca, 0xfe, 0xba, 0xbe]);
+    const reader = buf.getReader();
+    const result1 = reader.read(2);
+    const result2 = reader.read(2);
+
+    t.deepEqual(result1, Buffer.from([0xca, 0xfe]));
+    t.deepEqual(result2, Buffer.from([0xba, 0xbe]));
+  });
+
+  it('BufferReader.prototype.seek sets the position', (t) => {
+    const buf = Buffer.from([0xca, 0xfe, 0xba, 0xbe]);
+    const reader = buf.getReader();
+    const result1 = reader.read(2);
+    reader.seek(0);
+    const result2 = reader.read(2);
+
+    t.deepEqual(result1, Buffer.from([0xca, 0xfe]));
+    t.deepEqual(result2, Buffer.from([0xca, 0xfe]));
+  });
+});
+
+describe('Buffer.prototype.getCppReader', () => {
+  it('returns a BufferReader instance', (t) => {
+    const buf = Buffer.from([0xca, 0xfe, 0xba, 0xbe]);
+    const reader = buf.getCppReader();
+
+    t.equal(typeof reader.read, 'function');
+    t.equal(typeof reader.seek, 'function');
+  });
+
+  it('BufferReader.prototype.read gets n bytes from actual buffer', (t) => {
+    const buf = Buffer.from([0xca, 0xfe, 0xba, 0xbe]);
+    const reader = buf.getCppReader();
+    const result1 = reader.read(2);
+    const result2 = reader.read(2);
+
+    t.deepEqual(result1, Buffer.from([0xca, 0xfe]));
+    t.deepEqual(result2, Buffer.from([0xba, 0xbe]));
+  });
+
+  it('BufferReader.prototype.seek sets the position', (t) => {
+    const buf = Buffer.from([0xca, 0xfe, 0xba, 0xbe]);
+    const reader = buf.getCppReader();
+    const result1 = reader.read(2);
+    reader.seek(0);
+    const result2 = reader.read(2);
+
+    t.deepEqual(result1, Buffer.from([0xca, 0xfe]));
+    t.deepEqual(result2, Buffer.from([0xca, 0xfe]));
+  });
+});
