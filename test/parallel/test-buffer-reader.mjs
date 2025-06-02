c64aec3ff4652859fdec48bd15911ffb577bdc82
Edy Silva <edigleyssonsilva@gmail.com>
2025-06-02 17:35:50 -0300

    add test

--- test/parallel/test-buffer-reader.mjs
+++ test/parallel/test-buffer-reader.mjs
@@ -6,8 +6,8 @@ describe('Buffer.prototype.getReader', () => {
     const buf = Buffer.from([0xca, 0xfe, 0xba, 0xbe]);
     const reader = buf.getReader();
 
-    t.equal(typeof reader.read, 'function');
-    t.equal(typeof reader.seek, 'function');
+    t.assert.strictEqual(typeof reader.read, 'function');
+    t.assert.strictEqual(typeof reader.seek, 'function');
   });
 
   it('BufferReader.prototype.read gets n bytes from actual buffer', (t) => {
@@ -16,8 +16,8 @@ describe('Buffer.prototype.getReader', () => {
     const result1 = reader.read(2);
     const result2 = reader.read(2);
 
-    t.deepEqual(result1, Buffer.from([0xca, 0xfe]));
-    t.deepEqual(result2, Buffer.from([0xba, 0xbe]));
+    t.assert.deepStrictEqual(result1, Buffer.from([0xca, 0xfe]));
+    t.assert.deepStrictEqual(result2, Buffer.from([0xba, 0xbe]));
   });
 
   it('BufferReader.prototype.seek sets the position', (t) => {
@@ -27,8 +27,8 @@ describe('Buffer.prototype.getReader', () => {
     reader.seek(0);
     const result2 = reader.read(2);
 
-    t.deepEqual(result1, Buffer.from([0xca, 0xfe]));
-    t.deepEqual(result2, Buffer.from([0xca, 0xfe]));
+    t.assert.deepStrictEqual(result1, Buffer.from([0xca, 0xfe]));
+    t.assert.deepStrictEqual(result2, Buffer.from([0xca, 0xfe]));
   });
 });
 
@@ -37,8 +37,8 @@ describe('Buffer.prototype.getCppReader', () => {
     const buf = Buffer.from([0xca, 0xfe, 0xba, 0xbe]);
     const reader = buf.getCppReader();
 
-    t.equal(typeof reader.read, 'function');
-    t.equal(typeof reader.seek, 'function');
+    t.assert.strictEqual(typeof reader.read, 'function');
+    t.assert.strictEqual(typeof reader.seek, 'function');
   });
 
   it('BufferReader.prototype.read gets n bytes from actual buffer', (t) => {
@@ -47,8 +47,8 @@ describe('Buffer.prototype.getCppReader', () => {
     const result1 = reader.read(2);
     const result2 = reader.read(2);
 
-    t.deepEqual(result1, Buffer.from([0xca, 0xfe]));
-    t.deepEqual(result2, Buffer.from([0xba, 0xbe]));
+    t.assert.deepEqual(result1, Buffer.from([0xca, 0xfe]));
+    t.assert.deepEqual(result2, Buffer.from([0xba, 0xbe]));
   });
 
   it('BufferReader.prototype.seek sets the position', (t) => {
@@ -58,7 +58,7 @@ describe('Buffer.prototype.getCppReader', () => {
     reader.seek(0);
     const result2 = reader.read(2);
 
-    t.deepEqual(result1, Buffer.from([0xca, 0xfe]));
-    t.deepEqual(result2, Buffer.from([0xca, 0xfe]));
+    t.assert.deepEqual(result1, Buffer.from([0xca, 0xfe]));
+    t.assert.deepEqual(result2, Buffer.from([0xca, 0xfe]));
   });
 });
